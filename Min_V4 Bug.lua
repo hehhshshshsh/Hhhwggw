8dc6baf088819dd35c91f8ca16109ea44f7e034a
hehhshshshsh <160491764+hehhshshshsh@users.noreply.github.com>
2024-03-02 14:27:18 +0700

    Update Min_V4 Bug.lua

--- Min_V4 Bug.lua
+++ Min_V4 Bug.lua
@@ -290,7 +290,7 @@ ImageButton1.BorderSizePixel = 0
 ImageButton1.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
 ImageButton1.Size = UDim2.new(0, 50, 0, 50)
 ImageButton1.Draggable = true
-ImageButton1.Image = "rbxassetid://13717478897"
+ImageButton1.Image = "rbxassetid://16587781429"
 ImageButton1.MouseButton1Down:connect(function()
 game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
 game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
